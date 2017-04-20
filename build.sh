cd24c71dbbd60fb5dd6188a58512ed853d160e2b
Matthew Craig <matthew.craig@ncino.com>
2017-04-20 14:53:01 -0400

    PP-1501 Added requird dirs

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 src="$HOME/Font-Awesome/src"
-tmp="/tmp"
+tmp="tmp"
 components="$CIRCLE_ARTIFACTS/components"
 pattern_fonts="\/\* import fonts \*\/"
 pattern_url="url\(['\"](\.\.\/?)*((\/[[:alnum:]_\.\-]+)+)([\?#]+[[:alnum:]=&#_\.\-]+)?['\"]\)"
@@ -14,6 +14,9 @@ fa_fonts="font-awesome-font.css"
 fa_cmp="Define_Font_Awesome_Fonts.component"
 fa_url="url(\"{\!URLFOR(\$Resource.font_awesome, \'\2\')}\4\")"
 
+mkdir "tmp"
+mkdir $components
+cp "$fa_dist/$fa_archive" "$components/$fa_rsrc"
 sed -E "s/$pattern_url/$fa_url/g" "$fa_styles/$fa_fonts" | sed -E "s/&/&amp;/g" > "$tmp/$fa_fonts"
 sed -E "/$pattern_fonts/r $tmp/$fa_fonts" "$components/$fa_cmp" > "$tmp/$fa_cmp"
 sed -E "/$pattern_fonts/d" "$tmp/$fa_cmp" > "$components/$fa_cmp"
